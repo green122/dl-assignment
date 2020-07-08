f9fa00bbcb96800829e28987c2ba111e079f1b9c
Andrei Gerasimov <gr122n@gmail.com>
2020-07-08 21:42:11 +0200

    basic scaffolding

---
+++ src/components/Question.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Question = props => {
+  return (
+    <div>
+
+    </div>
+  );
+};
+
+Question.propTypes = {
+  
+};
+
+export default Question;
