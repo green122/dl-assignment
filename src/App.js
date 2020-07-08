34179100b97f1ea4eebcdff16326d31007a715f3
Andrei Gerasimov <gr122n@gmail.com>
2020-07-08 21:56:04 +0200

    adds fetching hook

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import {BrowserRouter, Route, Switch} from "react-router-dom";
 import './App.scss';
+import Surveys from "./pages/Surveys";
+import SurveysDetails from "./pages/SurveysDetails";
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Switch>
+          <Route exact={true} path="/" component={Surveys}/>
+          <Route exact={true} path="/survey/:id" component={SurveysDetails}/>
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/hooks/useFetching.js
@@ -0,0 +1,46 @@
+import {useEffect, useReducer} from "react";
+import {AxiosError} from 'axios';
+
+const dataFetchReducer = (state, action) => {
+  switch (action.type) {
+    case 'request':
+      return {...state, isLoading: true, error: undefined};
+    case 'success':
+      return {...state, isLoading: false, data: action.payload, error: undefined};
+    case 'failure':
+      return {...state, isLoading: false, error: action.payload};
+  }
+};
+
+export function useFetching(action, initialValue, deps) {
+
+  const initialState = {data: initialValue, isLoading: false};
+  const [state, dispatch] = useReducer(dataFetchReducer, initialState);
+
+  useEffect(() => {
+    let didCancel = false;
+    const fetchData = async () => {
+
+      dispatch({type: 'request'});
+
+      try {
+        const result = await action();
+        if (!didCancel) {
+          dispatch({type: 'success', payload: result});
+        }
+      } catch (error) {
+        if (!didCancel) {
+          dispatch({type: 'failure', payload: error});
+        }
+      }
+    };
+
+    fetchData();
+
+    return () => {
+      didCancel = true;
+    };
+  }, deps);
+
+  return state;
+}
