10eb05d093b06d76a9411abbeec8b7cc302c2354
Andrei Gerasimov <gr122n@gmail.com>
2020-07-08 22:14:17 +0200

    Added surveys fetching and surveys list component

---
+++ src/components/SurveysList.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const SurveysList = ({surveys}) => {
+  return (
+    <div>
+
+    </div>
+  );
+};
+
+SurveysList.propTypes = {
+  surveys: PropTypes.arrayOf(PropTypes.object)
+};
+
+export default SurveysList;
--- src/pages/Surveys.js
+++ src/pages/Surveys.js
@@ -1,16 +1,21 @@
-import React from 'react';
+import React, {Fragment} from 'react';
+import axios from 'axios';
 import PropTypes from 'prop-types';
+import {useFetching} from "../hooks/useFetching";
+import SurveysList from "../components/SurveysList";
 
 const Surveys = () => {
+  const loadSurveysData = useFetching(async () => {
+    const result = await axios.get('http://private-anon-3bbb60a48f-surveysmock.apiary-mock.com/api/surveys');
+    return result.data;
+  }, {}, []);
   return (
-    <div>
-
-    </div>
+    <Fragment>
+      {loadSurveysData.data && <SurveysList surveys={loadSurveysData.data.surveys}/>}
+    </Fragment>
   );
 };
 
-Surveys.propTypes = {
-
-};
+Surveys.propTypes = {};
 
 export default Surveys;
