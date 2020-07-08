f049265645d7ee0566aa6c2a855f50d56dcaa5fe
Andrei Gerasimov <gr122n@gmail.com>
2020-07-08 23:35:27 +0200

    Basic implementation of answering

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,15 +1,16 @@
 import React, {Fragment} from 'react';
 import PropTypes from 'prop-types';
 
-const Question = ({question}) => {
-  console.log(question.options);
+const Question = ({question, onSelectAnswer}) => {
+  // console.log(question.options);
   return (
     <div>
       <p>{question.title}</p>
       {question.options.map((option, index) =>
         <Fragment>
           <label>
-            <input name={question.id} onChange={e => console.log(e.currentTarget.value)} value={option} key={index}
+            <input name={question.id} onChange={e => onSelectAnswer(question.id, e.currentTarget.value)} value={option}
+                   key={index}
                    type="radio"/>
             {option}
           </label>
--- src/pages/SurveysDetails.jsx
+++ src/pages/SurveysDetails.jsx
@@ -1,21 +1,49 @@
-import React from 'react';
+import React, {useReducer} from 'react';
 import axios from 'axios';
 import PropTypes from 'prop-types';
 import {useFetching} from "../hooks/useFetching";
 import Question from "../components/Question";
 
+const questionsReducer = (state, action) => {
+  const {type, payload} = action;
+  switch (type) {
+    case 'set_questions_map':
+      return {
+        ...state, questions: payload.reduce((acc, question) => {
+          acc[question.id] = '';
+          return acc;
+        }, {})
+      }
+    case 'set_questions_answer':
+      return {...state, questions: {...state.questions, [payload.id]: payload.value}}
+  }
+}
+
+
 const SurveysDetails = ({match}) => {
+  const [state, dispatch] = useReducer(questionsReducer, {questions: {}});
+
   const {id} = match.params;
+
   const surveyDetailsData = useFetching(async () => {
     const result = await axios.get(`https://private-anon-3bbb60a48f-surveysmock.apiary-mock.com/api/surveys/${id}`);
-    return result.data;
-  }, {}, []);
-  console.log(surveyDetailsData.data);
+    dispatch({type: 'set_questions_map', payload: result.data.survey.questions})
+    return result.data.survey;
+  }, {}, [dispatch]);
+
+  const onSelectAnswer = (questionId, value) => dispatch({
+    type: 'set_questions_answer',
+    payload: {id: questionId, value}
+  });
+
+  const isValid = Object.values(state.questions).every(Boolean);
   return (
     <div>
-      {(surveyDetailsData?.data?.survey?.questions || []).map(
-        question => <Question key={question.id} question={question}/>
+      {(surveyDetailsData?.data?.questions || []).map(
+        question => <Question key={question.id} question={question} onSelectAnswer={onSelectAnswer}
+                              checkedAnswer={state.questions[question.id]}/>
       )}
+      <button disabled={!isValid}>Submit</button>
     </div>
   );
 };
